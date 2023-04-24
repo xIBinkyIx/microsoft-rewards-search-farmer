389601bd6e83ac6075b1c62d28316d62c4d57674
xIBinkyIx <122353512+xIBinkyIx@users.noreply.github.com>
2023-04-24 19:40:00 +0930

    Update microsoft.py
    added to where it will only one once per day, if you choose to add it to your start up apps. (I would recommend that so its one thing you can get done without having to worry about doing it)

--- microsoft.py
+++ microsoft.py
@@ -4,34 +4,54 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 import time
+import datetime
+import os
 
-options = webdriver.EdgeOptions()
-options.set_capability("unhandledPromptBehavior", "ignore")
-driver = webdriver.Edge(options=options)
+last_run_path = "last_run.txt"
 
-driver = webdriver.Edge()
-driver.maximize_window()
+if os.path.exists(last_run_path):
+    with open(last_run_path, "r") as f:
+        last_run_str = f.read().strip()
+    last_run = datetime.datetime.strptime(last_run_str, "%Y-%m-%d %H:%M:%S")
+else:
+    last_run = None
 
-driver.get("https://www.bing.com/")
+now = datetime.datetime.now()
 
-search_box: WebElement = driver.find_element(By.NAME, "q")
-search_box.send_keys("alphabet")
-search_box.send_keys(Keys.RETURN)
+if last_run and last_run.date() == now.date():
+    print("Script has already been run today. Exiting.")
+else:
 
-alphabet = list("abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789")
+    with open(last_run_path, "w") as f:
+        f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
 
-for letter in alphabet:
-    actions = ActionChains(driver)
-    actions.key_down(Keys.CONTROL).click().key_up(Keys.CONTROL).perform()
+    options = webdriver.EdgeOptions()
+    options.set_capability("unhandledPromptBehavior", "ignore")
+    driver = webdriver.Edge(options=options)
 
-    driver.switch_to.window(driver.window_handles[-1])
+    driver = webdriver.Edge()
+    driver.maximize_window()
+
+    driver.get("https://www.bing.com/")
 
     search_box: WebElement = driver.find_element(By.NAME, "q")
-    search_box.clear()
-    search_box.send_keys(letter)
+    search_box.send_keys("alphabet")
     search_box.send_keys(Keys.RETURN)
-    time.sleep(0.2)
 
-driver.switch_to.window(driver.window_handles[0])
+    alphabet = list("abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789")
 
-driver.quit()
+    for letter in alphabet:
+        actions = ActionChains(driver)
+        actions.key_down(Keys.CONTROL).click().key_up(Keys.CONTROL).perform()
+
+        driver.switch_to.window(driver.window_handles[-1])
+
+        search_box: WebElement = driver.find_element(By.NAME, "q")
+        search_box.clear()
+        search_box.send_keys(letter)
+        search_box.send_keys(Keys.RETURN)
+        time.sleep(0.2)
+
+    driver.switch_to.window(driver.window_handles[0])
+
+    driver.quit()

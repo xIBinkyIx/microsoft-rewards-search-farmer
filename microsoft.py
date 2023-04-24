164abed858e27ec7d76e0d80206ae601f466aafb
xIBinkyIx <122353512+xIBinkyIx@users.noreply.github.com>
2023-04-24 12:05:17 +0930

    Add files via upload

---
+++ microsoft.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+options = webdriver.EdgeOptions()
+options.set_capability("unhandledPromptBehavior", "ignore")
+driver = webdriver.Edge(options=options)
+
+driver = webdriver.Edge()
+driver.maximize_window()
+
+driver.get("https://www.bing.com/")
+
+search_box: WebElement = driver.find_element(By.NAME, "q")
+search_box.send_keys("alphabet")
+search_box.send_keys(Keys.RETURN)
+
+alphabet = list("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz12345678901234567890")
+
+for letter in alphabet:
+    actions = ActionChains(driver)
+    actions.key_down(Keys.CONTROL).click().key_up(Keys.CONTROL).perform()
+
+    driver.switch_to.window(driver.window_handles[-1])
+
+    search_box: WebElement = driver.find_element(By.NAME, "q")
+    search_box.clear()
+    search_box.send_keys(letter)
+    search_box.send_keys(Keys.RETURN)
+
+driver.switch_to.window(driver.window_handles[0])
+
+driver.quit()

279a6f6e5a7057ee23aba6aba116aed4a384313b
xIBinkyIx <122353512+xIBinkyIx@users.noreply.github.com>
2023-04-26 05:42:35 +0930

    Update microsoft.py
    changed the running once per day till I actually learn to use it properly

--- microsoft.py
+++ microsoft.py
@@ -4,57 +4,34 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 import time
-import datetime
-import os
 
-last_run_path = "last_run.txt"
+options = webdriver.EdgeOptions()
+options.set_capability("unhandledPromptBehavior", "ignore")
+driver = webdriver.Edge(options=options)
 
-if os.path.exists(last_run_path):
-    with open(last_run_path, "r") as f:
-        last_run_str = f.read().strip()
-    last_run = datetime.datetime.strptime(last_run_str, "%Y-%m-%d %H:%M:%S")
-else:
-    last_run = None
+driver = webdriver.Edge()
+driver.maximize_window()
 
-now = datetime.datetime.now()
+driver.get("https://www.bing.com/")
 
-if last_run and last_run.date() == now.date():
-    print("already ran. CLosing.")
-else:
-    with open(last_run_path, "w") as f:
-        f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
+search_box: WebElement = driver.find_element(By.NAME, "q")
+search_box.send_keys("alphabet")
+search_box.send_keys(Keys.RETURN)
 
-    options = webdriver.EdgeOptions()
-    options.set_capability("unhandledPromptBehavior", "ignore")
-    driver = webdriver.Edge(options=options)
+alphabet = list("abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789")
 
-    driver = webdriver.Edge()
-    driver.maximize_window()
+for letter in alphabet:
+    actions = ActionChains(driver)
+    actions.key_down(Keys.CONTROL).click().key_up(Keys.CONTROL).perform()
 
-    driver.get("https://www.bing.com/")
+    driver.switch_to.window(driver.window_handles[-1])
 
     search_box: WebElement = driver.find_element(By.NAME, "q")
-    search_box.send_keys("alphabet")
+    search_box.clear()
+    search_box.send_keys(letter)
     search_box.send_keys(Keys.RETURN)
+    time.sleep(0.2)
 
-    alphabet = list("abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789")
+driver.switch_to.window(driver.window_handles[0])
 
-    for letter in alphabet:
-        actions = ActionChains(driver)
-        actions.key_down(Keys.CONTROL).click().key_up(Keys.CONTROL).perform()
-
-        driver.switch_to.window(driver.window_handles[-1])
-
-        search_box: WebElement = driver.find_element(By.NAME, "q")
-        search_box.clear()
-        search_box.send_keys(letter)
-        search_box.send_keys(Keys.RETURN)
-        time.sleep(0.2)
-
-    driver.switch_to.window(driver.window_handles[0])
-
-    driver.quit()
-
-    # Close the file after writing
-    with open(last_run_path, "w") as f:
-        f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
+driver.quit()
